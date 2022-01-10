d1d6a0e6ad16f2b67fdf39ae9abccf14bc21a473
Vladimír Ksenič <38831957+Jolana1@users.noreply.github.com>
2022-01-10 21:50:46 +0100

    Add files via upload

--- gulpfile.js
+++ gulpfile.js
@@ -5,30 +5,34 @@ var uglify = require('gulp-uglify');
 var sass = require('gulp-sass');
 var minifyCss = require('gulp-clean-css');
 
-gulp.task('css', function() {
-    gulp.src('./jquery.pagepiling.css')
-        .pipe(sourcemaps.init())
-        .pipe(gulp.dest('./dist'))
-        .pipe(minifyCss({
-            compatibility: 'ie8',
-            advanced: false,
-            keepSpecialComments: '1'
-        }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist'));
+gulp.task('css', function () {
+	gulp.src('./dist/jquery.pagepiling.css')
+		.pipe(sourcemaps.init())
+		.pipe(gulp.dest('./dist'))
+		.pipe(minifyCss({
+			compatibility: 'ie8',
+			advanced: false,
+			keepSpecialComments: '1'
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./dist'));
 });
 
-gulp.task('js', function() {
-    gulp.src('./jquery.pagepiling.js')
-        .pipe(sourcemaps.init())
-        .pipe(gulp.dest('./dist'))
-        .pipe(uglify({
-            preserveComments: 'license'
-        }))
-        .pipe(rename({suffix: '.min'}))
-        .pipe(sourcemaps.write('.'))
-        .pipe(gulp.dest('./dist'));
+gulp.task('js', function () {
+	gulp.src('./jquery.pagepiling.js')
+		.pipe(sourcemaps.init())
+		.pipe(gulp.dest('./dist'))
+		.pipe(uglify({
+			preserveComments: 'license'
+		}))
+		.pipe(rename({
+			suffix: '.min'
+		}))
+		.pipe(sourcemaps.write('.'))
+		.pipe(gulp.dest('./dist'));
 });
 
 gulp.task('default', ['css', 'js']);
